5d192719632db04a427a5f1cb15726f717c06dee
Talgat Zharkynbay <talgat.zharkynbay@nu.edu.kz>
2025-05-03 16:57:35 +0600

    Update Seminar_3. Set. Dictionary.py

--- Python Basic/Seminar_3. Set. Dictionary.py
+++ Python Basic/Seminar_3. Set. Dictionary.py
@@ -74,6 +74,67 @@ while name_score != 'КОНЕЦ':
     
 print(grades)
 
+# Diet
+# Petya has decided to follow a new diet. He eats for dinner only those products which names have a certain length and begin with a certain letter. Help Petya determine what he can eat for dinner as a part of the new diet. The input is a comma-separated list of products. The program should output only those products that Petya can eat. If there are no such products, it should output the string "Have to go to the shop."
+#
+# INPUT FORMAT
+# A line of products' names, separated by a comma and space
+# The letter with that the names of the products allowed by a diet must begin.
+# An integer, the required length of the product name
+# Each entry from a new line.
+#
+# OUTPUT FORMAT
+# A string like "Petya can have the following for dinner tonight:" and then a comma-separated sequence of products' names (see example). Do not forget the dot in the end!
+# If there are no products satisfying conditions then the program outputs the string "Have to go to the shop."
+
+products = input().split(', ')
+letter = input()
+n = int(input())
+
+products_list = []
+
+for i, v in enumerate(products):
+    if v.startswith(letter) and len(v) == n:
+        products_list.append(v)
+        
+if len(products_list) == 0:
+    print("Have to go to the shop.")
+else:
+    print(f'Petya can have the following for dinner tonight: {", ".join(products_list)}.')
+
+# Battle of the bloggers
+# Bloggers Anya and Vanya decided to find out who can make the most money from advertising in two months. Every time someone gets an order for an advertisement, they write down in a special notebook the name of the person who got it and the price of the deal. Help them summarize the results of the competition.
+#
+# INPUT FORMAT
+# Strings with the name of the blogger who got the order (Anya or Vanya) and after a space, an integer, the number that represents the price of the deal. Each such string on a new line.
+# The input is terminated when the string "END" is entered.
+#
+# OUTPUT FORMAT
+# Name of the blogger who earned more.
+# If they both have earned the same amount, output the string "It's a tie."
+
+anya = 0
+vanya = 0
+
+info = input()
+
+while info != 'END':
+    name, summ = info.split()
+    
+    if name == 'Anya':
+        anya += int(summ)
+    else:
+        vanya += int(summ)
+    
+    info = input()
+
+if anya > vanya:
+    print('Anya')
+elif vanya > anya:
+    print('Vanya')
+else:
+    print("It's a tie.")
+
 
 
 
